7ab32d3c3b5f9f0f97350f6fa6fc8b918f0221f3
Frank Liao <liaofranku@gmail.com>
2021-03-23 13:13:07 -0700

    Refactor.

--- lib/main/api/rewards.ts
+++ lib/main/api/rewards.ts
@@ -15,29 +15,37 @@ class Rewards {
     if (!broadcasterId) {
       throw Error('No broadcaster ID found.')
     }
-    let response
-    try {
-      response = await api.call(
-        `helix/channel_points/custom_rewards?broadcaster_id=${broadcasterId}`
-      )
-    } catch (error) {
-      console.log(error)
-    }
+    const response = await api.call(
+      `helix/channel_points/custom_rewards?broadcaster_id=${broadcasterId}`
+    )
 
-    const rewards = response['data'][0]
-    const rewardsState = { rewards }
+    const rewardsState = responseToState(response)
     this.state.set(rewardsState)
-
     return rewardsState
   }
 }
 
-const DEFAULT_REWARDS_STATE = {
-  rewards: {},
-}
+const DEFAULT_REWARDS_STATE: RewardsState = []
 
-interface RewardsState {
-  rewards: any
+type RewardsState = {
+  id: string,
+  backgroundColor: string,
+
+}[]
+
+const responseToState = (response): RewardsState => {
+  const rewards = response['data']
+  return rewards.map(reward => {
+    const { id, title, image, default_image, background_color } = reward
+
+    return {
+      id,
+      title,
+      image,
+      defaultImage: default_image,
+      backgroundColor: background_color,
+    }
+  })
 }
 
 export default new Rewards()
--- lib/main/api/user.ts
+++ lib/main/api/user.ts
@@ -10,20 +10,13 @@ class User {
 
   async fetch() {
     const response = await api.call('helix/users')
-
-    const user = response['data'][0]
-    const userState = {
-      id: user.id,
-      displayName: user.display_name,
-      profileImageUrl: user.profile_image_url,
-    }
-    this.state.set(userState)
-
-    return userState
+    const state = responseToState(response)
+    this.state.set(state)
+    return state
   }
 }
 
-const DEFAULT_USER_STATE = {
+const DEFAULT_USER_STATE: UserState = {
   id: '',
   displayName: '',
   profileImageUrl: '',
@@ -33,6 +26,16 @@ interface UserState {
   id: string
   displayName: string
   profileImageUrl: string
+}
+
+const responseToState = (response) => {
+  const user = response['data'][0]
+
+  return {
+    id: user.id,
+    displayName: user.display_name,
+    profileImageUrl: user.profile_image_url,
+  }
 }
 
 export default new User()
